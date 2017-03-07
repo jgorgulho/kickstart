6503f9a79addd983fec8fe955a9d08a26ae3d640
José Gorgulho <jgorgulho@users.noreply.github.com>
2017-03-07 13:01:35 +0000

    Removed docker-compose bash_completion was giving 404 errors

--- workstationInstall.sh
+++ workstationInstall.sh
@@ -79,10 +79,6 @@ chown -R jgorgulho /home/jgorgulho/.bashrc
 echo "Installing Jekyll..."
 gem install jekyll
 
-echo "Installing docker compose..."
-curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
 echo "Installing docker compose bash completion..."
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
