5fbf9e315e3cefc6383969f3ae3ec69c2152e025
José Gorgulho <jgorgulho@gmail.com>
2017-06-20 21:28:07 +0100

    Moved installation file to install repo. This repo is still functional.

--- workstationInstall.sh
+++ workstationInstall.sh
@@ -4,22 +4,22 @@ echo "###############################################"
 echo "# Running Installation Script for Workstation #"
 echo "###############################################"
 
-#echo 'Setting delta rpm...'
-#echo "deltarpm=1" | sudo tee -a /etc/dnf/dnf.conf
-#
-#echo 'Updating system...'
-#sudo dnf update -y
-#
-#echo 'Setting repositories...'
-#
-#echo 'Installing rpmfusion...'
-#sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-#sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-#echo 'Installing GitHub Atom editor...'
-#sudo dnf install -y "https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm"
-#
-#echo 'Updating system...'
-#sudo dnf update -y
+echo 'Setting delta rpm...'
+echo "deltarpm=1" | sudo tee -a /etc/dnf/dnf.conf
+
+echo 'Updating system...'
+sudo dnf update -y
+
+echo 'Setting repositories...'
+
+echo 'Installing rpmfusion...'
+sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+echo 'Installing GitHub Atom editor...'
+sudo dnf install -y "https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm"
+
+echo 'Updating system...'
+sudo dnf update -y
 
 echo 'Installing big list of packages...'
 sudo dnf install -y abrt-desktop autoconf automake binutils bison chromium \
@@ -62,30 +62,30 @@ cd $HOME
 rm -Rf gnome-shell-extension-caffeine
 
 
-#echo 'Downloading hplip and lastpass for later install...'
-#mkdir /home/jgorgulho/toInstall
-#wget -O hplip.run "http://downloads.sourceforge.net/project/hplip/hplip/3.16.11/hplip-3.16.11.run?r=http%3A%2F%2Fhplipopensource.com%2Fhplip-web%2Finstall%2Finstall%2Findex.html&ts=1480467242&use_mirror=netassist"
-#wget -O lastpass.tar.bz2 "https://lastpass.com/lplinux.tar.bz2"
-#echo 'Moving hplip and lastpass to user folder...'
-#mv hplip.run /home/jgorgulho/toInstall
-#mv lastpass.tar.bz2 /home/jgorgulho/toInstall
-#chown -R jgorgulho /home/jgorgulho/toInstall
-#chown -R jgorgulho /home/jgorgulho/toInstall/*
-#
-#echo "Cloning dotfiles from github repo to root..."
-#git clone https://github.com/jgorgulho/dotfiles /root/.dotfiles
-#ln -sf /root/.dotfiles/.bashrc /root/.bashrc
-#echo "Cloning dotfiles from github repo to user..."
-#git clone https://github.com/jgorgulho/dotfiles /home/jgorgulho/.dotfiles
-#ln -sf /home/jgorgulho/.dotfiles/.bashrc /home/jgorgulho/.bashrc
-#chown -R jgorgulho /home/jgorgulho/.dotfiles
-#chown -R jgorgulho /home/jgorgulho/.dotfiles/.*
-#chown -R jgorgulho /home/jgorgulho/.bashrc
-#
-#echo "Installing Jekyll..."
-#gem install jekyll
-#
-#echo "Enabling tuned..."
-#systemctl enable tuned
-#echo "Enabling powertop..."
-#systemctl enable powertop
+echo 'Downloading hplip and lastpass for later install...'
+mkdir /home/jgorgulho/toInstall
+wget -O hplip.run "http://downloads.sourceforge.net/project/hplip/hplip/3.16.11/hplip-3.16.11.run?r=http%3A%2F%2Fhplipopensource.com%2Fhplip-web%2Finstall%2Finstall%2Findex.html&ts=1480467242&use_mirror=netassist"
+wget -O lastpass.tar.bz2 "https://lastpass.com/lplinux.tar.bz2"
+echo 'Moving hplip and lastpass to user folder...'
+mv hplip.run /home/jgorgulho/toInstall
+mv lastpass.tar.bz2 /home/jgorgulho/toInstall
+chown -R jgorgulho /home/jgorgulho/toInstall
+chown -R jgorgulho /home/jgorgulho/toInstall/*
+
+echo "Cloning dotfiles from github repo to root..."
+git clone https://github.com/jgorgulho/dotfiles /root/.dotfiles
+ln -sf /root/.dotfiles/.bashrc /root/.bashrc
+echo "Cloning dotfiles from github repo to user..."
+git clone https://github.com/jgorgulho/dotfiles /home/jgorgulho/.dotfiles
+ln -sf /home/jgorgulho/.dotfiles/.bashrc /home/jgorgulho/.bashrc
+chown -R jgorgulho /home/jgorgulho/.dotfiles
+chown -R jgorgulho /home/jgorgulho/.dotfiles/.*
+chown -R jgorgulho /home/jgorgulho/.bashrc
+
+echo "Installing Jekyll..."
+gem install jekyll
+
+echo "Enabling tuned..."
+systemctl enable tuned
+echo "Enabling powertop..."
+systemctl enable powertop
