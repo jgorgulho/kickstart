f21a97334561a87f8dbf2dbbb70192cfb6585656
José Gorgulho <jgorgulho@gmail.com>
2017-07-11 11:00:05 +0100

    Removed hp and lastpass install

--- serverInstall.sh
+++ serverInstall.sh
@@ -24,6 +24,8 @@ sudo dnf update -y
 sudo dnf install -y abrt-desktop autoconf automake binutils bison chromium \
 chrony cmake cockpit cockpit-bridge cockpit-docker cockpit-kubernetes \
 cockpit-networkmanager cockpit-pcp cockpit-shell cockpit-storaged cockpit-ws \
+cockpit-selinux cockpit-dock cockpit-dashboard cockpit-machines \
+cockpit-packagekit cockpit-sosreport cockpit-system \
 cowsay cups cups-filters dejavu-sans-mono-fonts deltarpm diffstat docker \
 docker-registry docker-vim doxygen fedora-dockerfiles flex \
 fortune-mod gcc gcc-c++ gdb gettext ghostscript git glibc-devel \
@@ -41,19 +43,10 @@ system-config-users systemtap tmux tuned unzip vim vim-common \
 vim-enhanced vim-filesystem *powerline* virt-install virt-manager \
 virt-top virt-viewer w3m youtube-dl fuse-sshfs
 
-echo 'Downloading hplip and lastpass for later install...'
-mkdir /home/jgorgulho/toInstall
-wget -O hplip.run "http://downloads.sourceforge.net/project/hplip/hplip/3.16.11/hplip-3.16.11.run?r=http%3A%2F%2Fhplipopensource.com%2Fhplip-web%2Finstall%2Finstall%2Findex.html&ts=1480467242&use_mirror=netassist"
-wget -O lastpass.tar.bz2 "https://lastpass.com/lplinux.tar.bz2"
-echo 'Moving hplip and lastpass to user folder...'
-mv hplip.run /home/jgorgulho/toInstall
-mv lastpass.tar.bz2 /home/jgorgulho/toInstall
-chown -R jgorgulho /home/jgorgulho/toInstall
-chown -R jgorgulho /home/jgorgulho/toInstall/*
-
 echo "Cloning dotfiles from github repo to root..."
 git clone https://github.com/jgorgulho/dotfiles /root/.dotfiles
 ln -sf /root/.dotfiles/.bashrc /root/.bashrc
+ln -sf /root/.dotfiles/.dfm /root/.dfm
 echo "Cloning dotfiles from github repo to user..."
 git clone https://github.com/jgorgulho/dotfiles /home/jgorgulho/.dotfiles
 ln -sf /home/jgorgulho/.dotfiles/.bashrc /home/jgorgulho/.bashrc
@@ -61,8 +54,8 @@ chown -R jgorgulho /home/jgorgulho/.dotfiles
 chown -R jgorgulho /home/jgorgulho/.dotfiles/.*
 chown -R jgorgulho /home/jgorgulho/.bashrc
 
-echo "Installing Jekyll..."
-gem install jekyll
+#echo "Installing Jekyll..."
+#gem install jekyll
 
 echo "Enabling tuned..."
 systemctl enable tuned
